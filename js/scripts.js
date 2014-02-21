8adbc44c928d3e90e578210f3bd180f1a7b3200a
Angela Yen & Jason Hamilton <student@epicodus.com>
2014-02-21 14:24:42 -0800

    Scrabble w. object

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,29 @@
 var letterValues = function(letter) {
-  var onePointLetters = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'];
-  var twoPointLetters = ['D', 'G'];
-  var threePointLetters = ['B','C','M','P'];
-  var fourPointLetters = ['F','H','V','W','Y']; 
   letter = letter.toUpperCase();
-
-  if (onePointLetters.indexOf(letter) > -1) {
-    return 1;
-  } else if (twoPointLetters.indexOf(letter) > -1) {
-    return 2;
-  } else if(threePointLetters.indexOf(letter) > -1) {
-    return 3;
-  } else if (fourPointLetters.indexOf(letter) > -1) {
-    return 4;
-  } else if (letter === 'K') {
-    return 5;
-  } else if (letter === 'J' || letter === 'X') {
-    return 8;
-  } else if (letter === 'Q' || letter === 'Z') {
-    return 10;
-  } else {
-    return 0;
-  }
+  var alphabet = { 'A' :1, 'E':1, 'I':1, 'O':1, 'U':1, 'L':1, 'N':1, 'R':1, 'S':1, 'T':1,
+    'D' : 2, 'G': 2,
+    'B' : 3, 'C': 3, 'M': 3, 'P': 3,
+    'F': 4, 'H' : 4, 'V': 4, 'W': 4, 'Y': 4,
+    'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10};
+    return alphabet[letter];
 };
 
 
 var scrabble = function(word) {
+  var regExp = /[a-zA-Z]/;
   if (word === '') {
     return 0;
-  } else {
 
+  } else if (regExp.test(word) === false) {
+    alert("Please enter a real word.");
+    
+  } else {
     var letter = word.substr(0,1);
     var newWord = word.substr(1);
-
-    return letterValues(letter) + scrabble(newWord);
   }
+  
+  return letterValues(letter) + scrabble(newWord);
 };
-
-
 
 
 $(function() {
